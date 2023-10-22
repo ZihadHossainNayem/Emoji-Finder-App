f9cb0c6451920bf676c0b00434bc353413dbbba2
Zihad Hossain Nayem <zihadhossainnayem@gmail.com>
2023-10-22 18:48:01 +0600

    emoji name on hover

--- src/app/components/EmojiShowcase.jsx
+++ src/app/components/EmojiShowcase.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 export const EmojiShowcase = () => {
   /* manage the  state for the fetched emojis here */
   const [emojis, setEmojis] = useState([]);
+  const [emojiName, setEmojiName] = useState(null);
   /* data fetching when the component mounts */
   useEffect(() => {
     const fetchData = async () => {
@@ -22,14 +23,26 @@ export const EmojiShowcase = () => {
   }, []);
 
   return (
-    <div className="md:px-8 px-4 md:py-6 py-4 flex flex-wrap gap-2 bg-[#f7f7f7]">
+    <div className="md:px-6 px-2 md:py-6 py-4 flex flex-wrap gap-3">
       {emojis.map((emoji, index) => (
-        <span
+        <div
           key={index}
-          className="text-3xl h-12 min-w-12 p-2 border rounded flex items-center justify-center"
+          className="md:text-3xl text-xl p-2 flex items-center justify-center 
+          md:h-16 md:min-w-16 h-12 min-w-12 border rounded relative
+          hover:bg-gray-100"
+          onMouseEnter={() => setEmojiName(emoji)}
+          onMouseLeave={() => setEmojiName(null)}
         >
-          {emoji.character}
-        </span>
+          <span>{emoji.character}</span>
+          {emojiName === emoji && (
+            <span
+              className="absolute top-full left-full text-sm bg-gray-600 text-white 
+            p-2 rounded whitespace-nowrap z-10 "
+            >
+              {emoji.unicodeName}
+            </span>
+          )}
+        </div>
       ))}
     </div>
   );
